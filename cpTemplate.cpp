a63f41d4ba034cc29fe55e7d388cdc79da033f38
dev-saiful <saifulcseian@gmail.com>
2023-06-17 10:29:00 +0600

    cp template

---
+++ cpTemplate.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i,a,b) for(int i=a;i<=b;i++)
+using namespace std;
+typedef long long ll;
+typedef vector<int> v;
+typedef pair<int,int> pi;
+const int N = 10e5+10;
+int main()
+{
+	// freopen("input.txt", "r", stdin);
+	// freopen("output.txt", "w", stdout);
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+
+	return 0;
+}
